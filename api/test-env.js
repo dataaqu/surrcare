99c736e222469e9631cd399cc32ce3ffe84abbb8
Data <macintoshi@Datas-MacBook-Pro.local>
2025-12-05 00:17:07 +0400

    fix sending error

--- api/test-env.js
+++
@@ -1,9 +0,0 @@
-// api/test-env.js
-export default function handler(req, res) {
-  res.json({
-    hasResendKey: !!process.env.RESEND_API_KEY,
-    hasAdminEmail: !!process.env.ADMIN_EMAIL,
-    hasKvUrl: !!process.env.KV_REST_API_URL,
-    hasKvToken: !!process.env.KV_REST_API_TOKEN,
-  });
-}
